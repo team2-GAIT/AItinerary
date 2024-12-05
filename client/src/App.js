eb606a697bcc769a810bc362590da5c21153066e
zakmin11 <zmineiko@gmail.com>
2024-12-05 01:28:13 -0500

    working launchpoint

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
-
-// to get routes to work:
-// in terminal, run the following in the root dir of the proj (i.e. /travel-app):
-// "npm i -D react-router-dom@latest" 
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import NavBar from './components/NavBar';
 import Homepage from './pages/Homepage';
@@ -12,20 +8,18 @@ import Homepage from './pages/Homepage';
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        {/* NavBar will appear on ALL pages */}
-        <NavBar />
-        
-        <div className="content">
-          <Routes>
-            <Route path="/" element={<Homepage />} />
-            
-            {/* Add more routes as needed */}
-          </Routes>
-        </div>
+    <div className="App">
+      {/* NavBar will appear on ALL pages */}
+      <NavBar />
+      
+      <div className="content">
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          
+          {/* Add more routes as needed */}
+        </Routes>
       </div>
-    </Router>
+    </div>
   );
 }
 
