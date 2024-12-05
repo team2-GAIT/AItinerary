f98782e97623b72377ead65bcc2b90c30e2ccd88
zakmin11 <zmineiko@gmail.com>
2024-12-05 00:50:08 -0500

    this is all messed up

---
+++ client/src/components/NavBar.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/NavBar.css'; 
+import logo from '../assets/logo.png'; 
+
+const NavBar = () => {
+  return (
+    <nav className="navbar">
+      <div className="navbar-logo">
+        <Link to="/">
+          <img src={logo} alt="Logo" className="logo" />
+        </Link>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
---
+++ client/src/pages/Homepage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import "../styles/Homepage.css"
+
+function Homepage(props) {
+    return (
+        <div>
+            <h2 className={'MainText'}>This is the homepage</h2>
+        </div>
+    );
+}
+
+export default Homepage;
